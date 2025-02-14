732c57be4ab97f7d1b7262df188e8631361d2831
josezurita21 <10511@holbertonstudents.com>
2025-02-14 20:07:07 +0000

    10

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * main - entry point for program
+ *
+ * Description: print a string using a character array
+ *
+ * Return: always 1 (success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
