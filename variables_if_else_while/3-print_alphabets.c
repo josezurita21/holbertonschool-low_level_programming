2f7dcfd526cc269fdc2f109fac81ba68220e647f
josezurita21 <10511@holbertonstudents.com>
2025-02-17 23:26:10 +0000

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		putchar(d);
+	}
+
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
